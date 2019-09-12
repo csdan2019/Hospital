13475f2dbd0e6169abf85f95e145a6113dd9b722
csdan2019 <49932259+csdan2019@users.noreply.github.com>
2019-09-11 21:52:51 -0700

    update port

--- dbcon.js
+++ dbcon.js
@@ -1,10 +1,10 @@
 let mysql = require('mysql');
 let pool = mysql.createPool({
 	connectionLimit : 10,
-	host : 'localhost',
-	user : 'backpack',
-	password : 'password',
-	database : 'hospitalDB'
+	host : 'nkpl8b2jg68m87ht.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+	user : 'emz9brlh70p8cpot	',
+	password : 'n49u0hsw7ich0mxp',
+	database : 'g96cthu7a4gm7clz'
 });
 
 module.exports.pool = pool;
