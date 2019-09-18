9e39593f41493cc77475969294a3104d8d33c0fe
csdan2019 <49932259+csdan2019@users.noreply.github.com>
2019-09-17 19:45:45 -0700

    update db

--- dbcon.js
+++ dbcon.js
@@ -1,10 +1,10 @@
 let mysql = require('mysql');
 let pool = mysql.createPool({
 	connectionLimit : 10,
-	host : 'nkpl8b2jg68m87ht.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-	user : 'emz9brlh70p8cpot	',
-	password : 'n49u0hsw7ich0mxp',
-	database : 'g96cthu7a4gm7clz'
+	host : 'classmysql.engr.oregonstate.edu',
+	user : 'cs340_tonthada',
+	password : '8034',
+	database : 'cs340_tonthada'
 });
 
 module.exports.pool = pool;
